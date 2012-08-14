ba654b3b671d9036b533572d2d0002fcea9dbd80
Christian Hergert <chris@dronelabs.com>
2012-08-14 02:16:13 -0700

    util: add util for clock functions.

---
+++ uber/uber-util.h
@@ -0,0 +1,30 @@
+/* uber-util.h
+ *
+ * Copyright (C) 2012 Christian Hergert <chris@dronelabs.com>
+ *
+ * This file is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef UBER_UTIL_H
+#define UBER_UTIL_H
+
+#include <glib.h>
+
+G_BEGIN_DECLS
+
+gdouble uber_get_current_time (void);
+
+G_END_DECLS
+
+#endif /* UBER_UTIL_H */
