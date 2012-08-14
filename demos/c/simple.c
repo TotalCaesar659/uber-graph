78432461b053b990c890b6edaf6b33f1ff6bdab0
Christian Hergert <chris@dronelabs.com>
2012-08-14 01:58:24 -0700

    simple: add a basic model/line to render.

--- demos/c/simple.c
+++ demos/c/simple.c
@@ -8,10 +8,13 @@ main (gint   argc,
    GtkWindow *window;
    GtkWidget *graph;
    UberRenderer *renderer;
+   UberModel *model;
 
    gtk_init(&argc, &argv);
 
+   model = uber_model_memory_new(1, G_TYPE_DOUBLE);
    renderer = g_object_new(UBER_TYPE_RENDERER_LINE, NULL);
+   uber_renderer_line_append(UBER_RENDERER_LINE(renderer), model, 0);
    graph = g_object_new(UBER_TYPE_GRAPH,
                         "renderer", renderer,
                         "visible", TRUE,
